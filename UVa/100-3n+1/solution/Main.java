5c49543fdeb7c6a84c843ba065629bd0f2ab4e63
Riley Dawson <riley@ditguardian.com>
2015-10-14 15:42:18 -0600

    Accepted solution

--- UVa/100-3n+1/solution/Main.java
+++ UVa/100-3n+1/solution/Main.java
@@ -1,37 +1,43 @@
+package solution;
 import java.util.*;
 import java.io.*;
 
 // UVa Online judge problem 100 - The 3n + 1 problem.
-class threeN {
+// NOTE: class name has to be "Main" for proper compilation.
+class Main {
 
 	public static void main(String[] args){
-		threeN work = new threeN();
-		work.Begin();
-	}
-	 void Begin() {
+		
 		// Initialize io objects
 		Scanner in = new Scanner(System.in);
 		PrintWriter out = new PrintWriter(System.out, true);
 		
 		// Create a hasmap dictionary of cycle lengths for each number to limit amount of times the length is calculated.
-		Map<Integer, Integer> cycleMap =  new HashMap<Integer, Integer>();
+		Map<Long, Long> cycleMap =  new HashMap<Long, Long>();
 		
 		// Run for as long as input is an integer
-		while(in.hasNextInt()){
-			int i = in.nextInt();
-			int j = in.nextInt();
-			int max = 0;
+		while(in.hasNextLong()){
+			
+			long i= 0;
+			long j= 0;
+			i = in.nextLong();
+			if(in.hasNextInt()){
+			j = in.nextLong();
+			} else{
+				break;
+			}
+			long max = 0;
 			
 			//Determine start and end numbers
-			int start = (i>j)?j:i;
-			int finish = (i>j)?i:j;
+			long start = (i>j)?j:i;
+			long finish = (i>j)?i:j;
 			
 			
 
 			// for every number between i and j, first see if cycle length is in cycleMap, if so print that value
 			// if not, calculate cycle length for the number.
-			for (int target = start; target <= finish; target++){
-				int cycleLength = -1;
+			for (long target = start; target <= finish; target++){
+				long cycleLength = -1;
 				
 				// Check if the current target's cycle length has already been calculated
 				if(cycleMap.containsKey(target)){
@@ -49,17 +55,18 @@ class threeN {
 				max = (max<cycleLength)? cycleLength:max;
 				
 			}
-			// Display output
-			out.printf("%d %d %d\n",start,finish,max);
-
+			// Display output 
+			// System.out.println(start + " "+ finish + " " + max);
+			// NOTE: The above is an incorrect output, the output order must match the input order (I fell for this gotcha). 
+			System.out.println(i + " "+ j + " " + max);
 		}
 
 		in.close();
 		out.close();
-		
+		System.exit(0);
 	}
 	// Calculates the cycle length for a given target, for the 3n+1 problem
-	private static int cycleLength(int target){
+	private static long cycleLength(long target){
 		int length = 1;
 		while(target != 1){
 			// If the number is even, divide by two, else multiply by three and add 1.
