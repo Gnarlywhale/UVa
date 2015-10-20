e05e361096eceed92e6e54bf3b093f04da3950cd
Riley Dawson <riley@ditguardian.com>
2015-10-20 11:47:32 -0600

    Finished 102 - Ecological Bin Sorting

--- UVa/102 - Ecological Bin Packing/Main.java
+++ UVa/102 - Ecological Bin Packing/Main.java
@@ -1,72 +1,69 @@
 import java.util.Scanner;
 
+// UVa Online judge problem 102 - Ecological Bin Packing
+// NOTE: class name has to be "Main" for proper compilation.
 class Main {
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
+
+    // Store the bottle positions
     int[] bins = new int[9];
     while (in.hasNext()){
       bins = fillBins(in);
-      /*
-        0 1 2
-        4 5 6
-        7 8 9
+      /* Representation of bottle amounts per bin.
+           1      2       3
+        B G C | B G C | B G C
+        0 1 2 | 3 4 5 | 6 7 8
       */
-      /*
-        1 2 3
-        4 5 6
-        7 8 9
-
-        B:4+7
-        G:7
-        C:3+9
-      */
-
+      // Store results
       int[] results = new int[6];
-      results[0] = moveCount(bins, 0, 5, 9);
-      results[1] = moveCount(bins, 0, 6, 8);
-      results[2] = moveCount(bins, 1, 4, 9);
-      results[3] = moveCount(bins, 1, 6, 7);
-      results[4] = moveCount(bins, 2, 4, 8);
-      results[5] = moveCount(bins, 2, 5, 7);
 
-      // Find lowest
+      // Find move counts for each possible orientation
+      results[0] = moveCount(bins, 1,5,6); //GCB
+      results[1] = moveCount(bins, 1,3,8); //GBC
+      results[2] = moveCount(bins, 2,4,6); //CGB
+      results[3] = moveCount(bins, 2,3,7); //CBG
+      results[4] = moveCount(bins, 0,4,8); //BGC
+      results[5] = moveCount(bins, 0,5,7); //BCG
+
+      // Find Lowest move count
       int lowest = 0;
       for (int i = 1; i < 6; i++){
         if (results[i] <= results[lowest]){
           lowest = i;
         }
       }
-      // for(int res: results){
-      //   System.out.println(res);
-      // }
+      // Display answer
       switch (lowest){
-        case 0: System.out.println("BGC "+results[lowest]);
+        case 0: System.out.println("GCB "+results[lowest]);
                 break;
-        case 1: System.out.println("BCG "+results[lowest]);
+        case 1: System.out.println("GBC "+results[lowest]);
                 break;
-        case 2: System.out.println("GBC "+results[lowest]);
+        case 2: System.out.println("CGB "+results[lowest]);
                 break;
         case 3: System.out.println("CBG "+results[lowest]);
                 break;
-        case 4: System.out.println("GCB "+results[lowest]);
+        case 4: System.out.println("BGC "+results[lowest]);
                 break;
-        case 5: System.out.println("CGB "+results[lowest]);
+        case 5: System.out.println("BCG "+results[lowest]);
                 break;
 }
     }
 
   }
 
-  private static int moveCount(int[] bins, int bIdx, int gIdx, int cIdx){
+  // Calculate the required amount of moves to
+  private static int moveCount(int[] bins, int x, int y, int z){
     int moves = 0;
+    // Ignore the indexes of bottles that don't need to be moved
     for (int i = 0; i < 9; i++){
-      if (i != bIdx && i != gIdx && i != cIdx){
+      if (i != x && i != y && i != z){
         moves += bins[i];
       }
     }
     return moves;
   }
-
+  // Read in initial bin orientation.
   private static int[] fillBins(Scanner in){
     int[] bins = new int[9];
     for (int bin = 0; bin < 9; bin++){
