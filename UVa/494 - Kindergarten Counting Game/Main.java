3af0baba62d94f124dc0dfa6efc0009d79b5db7e
Riley Dawson <riley@ditguardian.com>
2015-10-28 09:39:28 -0600

    494 - Kindergarten Counting Game

---
+++ UVa/494 - Kindergarten Counting Game/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// UVa Online judge problem 494 - Kindergarten Counting Game
+// I'm doubtful Kindergarten kids would know to count "\\\outl///ived"
+// as two words....
+// NOTE: class name has to be "Main" for proper compilation.
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    while(in.hasNext()){
+        // Replace all non-whitespace, non-alphabet characters with whitespace.
+        // Remove duplicate whitespace
+        // Remove preceding and trailing whitespace
+        // Split the string on each whitespace character and count the result.
+        String[] words = in.nextLine().replaceAll("[^a-zA-Z ]", " ").replaceAll("\\s+"," ").trim().split(" ");
+        System.out.println(words.length);
+    }
+  }
+}
